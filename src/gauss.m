cab3ae833689752bf5602afc150ccc378c637745
青袍君 <51046846+Ultrasty@users.noreply.github.com>
2020-11-30 05:16:02 -0600

    更改了gauss的算法

--- src/gauss.m
+++ src/gauss.m
@@ -1,16 +1,7 @@
 function filter = gauss(sigma,n)
-%GAUSS
-filter = zeros(1,n);
-Normalization = 1/(exp(-(1-(n+1)/2)^2/(2*sigma^2))/(sigma*sqrt(2*pi)));
-count = 0;
-for i=1 : n
-    filter(i) = exp(-(i-(n+1)/2)^2/(2*sigma^2))/(sigma*sqrt(2*pi))*Normalization;
-    count = count + filter(i);
-end
-
-for i=1 : n
-    filter(i) = filter(i)/count;% 归一化
-end
-
+%GAUSS 因为matlab对矩阵运算做了优化，因此不需要用for循环
+filter = 1-(n+1)/2:n-(n+1)/2;
+filter= exp(-filter.^2/(2*sigma^2));
+filter= filter/sum(filter);% 归一化
 end
 
